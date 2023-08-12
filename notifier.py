e4b2d4db78906d70a8f2edf004463ae083571fcc
Vishal0129 <vishalsai010304@gmail.com>
2023-08-12 18:35:07 +0530

    encounter notification done

--- notifier.py
+++ notifier.py
@@ -2,6 +2,11 @@ import cx_Oracle
 from datetime import datetime, timedelta
 import time
 import json
+import threading
+import os
+import cv2
+import requests
+import base64
 
 class Database:
     def __init__(self, username, password, host, port, service_name):
@@ -10,7 +15,7 @@ class Database:
 
     def get_unique_encounters(self, time_period_minutes):
         query = f"""
-            SELECT e1.NAME, e1.CONFIDENCE, e1.TIMESTAMP, e1.IMAGE, e1.CAMERASOCKETURL, e1.LOCATION
+            SELECT e1.NAME, e1.CONFIDENCE, e1.TIMESTAMP, e1.IMAGE, e1.CAMERASOCKETURL, e1.LOCATION, e1.CAMERA_ID
             FROM encounters e1
             JOIN (
                 SELECT NAME, MAX(TIMESTAMP) AS max_timestamp
@@ -26,12 +31,32 @@ class Database:
             
         return results
 
+previous_encounters = []
 
+def clear_encounters():
+    global previous_encounters
+    while True:
+        time.sleep(300)
+        previous_encounters = []
 
+def send_encounters(encounters_list):
+    try:
+        images = []
+        for encounter in encounters_list:
+            image = cv2.imread('encounters/' + encounter["image"] + '.jpg')
+            image_data = cv2.imencode('.jpg', image)[1].tobytes()
+            images.append(base64.b64encode(image_data).decode('utf-8'))
 
-
-
-
+        for i in range(len(encounters_list)):
+            encounters_list[i]["image"] = images[i]
+        print(type(encounters_list))
+        response = requests.post("http://192.168.0.100:3001/notify", json=encounters_list)
+        if response.status_code == 200:
+            print("Encounters sent successfully")
+        else:
+            print("Failed to send encounters")
+    except Exception as e:
+        print("Error sending encounters:", e)
 
 if __name__ == "__main__":
     db = Database(username="aimlb4",
@@ -42,21 +67,28 @@ if __name__ == "__main__":
 
     time_period_minutes = 5  # Adjust the time period as needed
 
+    encounter_clearer_thread = threading.Thread(target=clear_encounters)
+    encounter_clearer_thread.daemon = True
+    encounter_clearer_thread.start()
+
+    size = len(os.listdir("encounters"))
+    column_names = ["name", "confidence", "timestamp", "image", "camerasocketurl", "location", "camera_id"]
     while True:
-        encounters = db.get_unique_encounters(time_period_minutes)
-        encounter_list = []
-        for encounter in encounters:
-            # Assuming you have a list of column names to work with
-            column_names = ["NAME", "CONFIDENCE", "TIMESTAMP", "IMAGE", "CAMERASOCKETURL", "LOCATION"]
-            encounter_details = {column: value for column, value in zip(column_names, encounter)}
+        new_size = len(os.listdir("encounters"))
+        if size != new_size:
+            encounters = db.get_unique_encounters(time_period_minutes)
+            encounter_list = []
+            for encounter in encounters:
+                # Assuming you have a list of column names to work with
+                encounter_details = dict(zip(column_names, encounter))
+                # print("Encounter details:", encounter_details)
+                if encounter_details["name"] not in previous_encounters:
+                    previous_encounters.append(encounter_details["name"])
+                    # Append the encounter details to the list
+                    encounter_list.append(encounter_details)
 
-            # Append the encounter details to the list
-            encounter_list.append(encounter_details)
-
-        # Convert the list of encounters to JSON format
-        encounters_json = json.dumps(encounter_list)
-
-        # Print the JSON results
-        print(encounters_json)
-
-        time.sleep(60)
+            if len(encounter_list) > 0:
+                print("Encounters:", encounter_list)
+                print("Previous encounters:", previous_encounters)
+                send_encounters(encounter_list)
+            # print("Previous encounters:", previous_encounters)
